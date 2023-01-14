0ca53a1255f790a39eb345b35c22fc5bde673bf9
Abraham Asrat <abrishsambong@gmail.com>
2023-01-14 14:23:40 +0300

    goood

---
+++ date.sh
@@ -0,0 +1,7 @@
+echo "We are in the year = $(date +%Y)"
+echo "We are in the month = $(date +%b)"
+echo "Current Day of the month = $(date +%d)"
+echo "current time in 12 hour format = $(date +%r)"
+echo Hours = $(date +%H)
+echo Minutes = $(date +%M)
+echo Seconds = $(date +%S)
